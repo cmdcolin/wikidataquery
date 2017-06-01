e6b4f8a5ee2564cd7facc2a02bcdce2b212e581e
Colin <colin.diesh@gmail.com>
2017-06-01 15:13:09 -0400

    Add special genes and operons queries

--- js/Store/SeqFeature/SPARQL.js
+++ js/Store/SeqFeature/SPARQL.js
@@ -71,20 +71,6 @@ function (
                 'Accept': 'application/json',
                 'X-Requested-With': null
             };
-            this.queryTemplate = "PREFIX wdt: <http://www.wikidata.org/prop/direct/> \
-                PREFIX wd: <http://www.wikidata.org/entity/> \
-                PREFIX qualifier: <http://www.wikidata.org/prop/qualifier/> \
-                SELECT ?start ?end ?uniqueID ?strand ?uri ?entrezGeneID ?name ?description ?refSeq \
-                WHERE { \
-                    ?featureType wdt:P279 {featureType}; wdt:P703 ?strain; wdt:P351 ?uniqueID; wdt:P351 ?entrezGeneID; wdt:P2393 ?name; rdfs:label ?description; wdt:P644 ?start; wdt:P645 ?end; wdt:P2548 ?wdstrand ; p:P644 ?chr. \
-                  OPTIONAL { \
-                        ?chr qualifier:P2249 ?refSeq. \
-                          FILTER(LANG(?description) = \"en\"). \
-                    } \
-                  FILTER(?refSeq = \"{ref}\") ?strain wdt:P685 '{organism}'. bind( IF(?wdstrand = wd:Q22809680, '1', '-1') as ?strand). bind(str(?featureType) as ?uri). filter ( !(xsd:integer(?start) > {end} || xsd:integer(?end) < {start})) \
-                }";
-
-            console.log(thisB._makeQuery(query));
 
             xhr.get(this.url + '?' + ioQuery.objectToQuery({ query: thisB._makeQuery(query) }), {
                 headers: headers,
