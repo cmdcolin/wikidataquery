c821b064141bb68faf56f737de95582817e96c5e
Colin <colin.diesh@gmail.com>
2017-06-01 14:32:08 -0400

    Add eslint

--- js/Store/SeqFeature/SPARQL.js
+++ js/Store/SeqFeature/SPARQL.js
@@ -7,7 +7,7 @@ define([
     'JBrowse/Store/SeqFeature/SPARQL',
     'JBrowse/Store/LRUCache'
 ],
-function(
+function (
     declare,
     lang,
     array,
@@ -17,23 +17,18 @@ function(
     LRUCache
 ) {
     return declare(SPARQL, {
-        constructor: function() {
-            console.log('here');
-            
-        },
-
-        getFeatures: function( query, featCallback, finishCallback, errorCallback ) {
+        getFeatures: function (query, featCallback, finishCallback, errorCallback) {
             var thisB = this;
             var cache = this.featureCache = this.featureCache || new LRUCache({
                 name: 'wikiDataFeatureCache',
-                fillCallback: dojo.hitch( this, '_readChunk' ),
-                sizeFunction: function( features ) {
+                fillCallback: dojo.hitch(this, '_readChunk'),
+                sizeFunction: function (features) {
                     return features.length;
                 },
                 maxSize: 100000 // cache up to 100,000 BAM features
             });
-            query.toString = function() {
-                return query.ref+','+query.start+','+query.end;
+            query.toString = function () {
+                return query.ref + ',' + query.start + ',' + query.end;
             };
             var chunkSize = 100000;
 
@@ -44,39 +39,37 @@ function(
             var chunksProcessed = 0;
             var haveError = false;
             for (var start = s; s < e; s += chunkSize) {
-                var chunk = { ref: query.ref, start: s, end: s+chunkSize };
-                chunk.toString = function() {
-                    return query.ref+','+query.start+','+query.end;
-                }
+                var chunk = { ref: query.ref, start: s, end: s + chunkSize };
+                chunk.toString = function () {
+                    return query.ref + ',' + query.start + ',' + query.end;
+                };
                 chunks.push(chunk);
             }
-            
-            array.forEach( chunks, function( c ) {
-                cache.get( c, function( f, e ) {
-                    if( e && !haveError )
-                        errorCallback(e);
-                    if(( haveError = haveError || e )) {
+
+            array.forEach(chunks, function (c) {
+                cache.get(c, function (f, e) {
+                    if (e && !haveError)                        {errorCallback(e);}
+                    if ((haveError = haveError || e)) {
                         return;
                     }
-                    var feats = thisB._resultsToFeatures( f, function(feature) {
-                        if( feature.get('start') > query.end ) // past end of range, can stop iterating
-                            return;
-                        else if( feature.get('end') >= query.start ) // must be in range
-                            featCallback( feature );
+                    var feats = thisB._resultsToFeatures(f, function (feature) {
+                        if (feature.get('start') > query.end) // past end of range, can stop iterating
+                            {return;}                        else if (feature.get('end') >= query.start) // must be in range
+                            {featCallback(feature);}
                     });
 
-                    if( ++chunksProcessed == chunks.length ) {
+                    if (++chunksProcessed == chunks.length) {
                         finishCallback();
                     }
                 });
             });
         },
-        _readChunk: function(query, callback) {
+        _readChunk: function (query, callback) {
             var thisB = this;
             var backoff = 200;
             var headers = {
-                "Accept": "application/json",
-                "X-Requested-With": null
+                'Accept': 'application/json',
+                'X-Requested-With': null
             };
             this.queryTemplate = "PREFIX wdt: <http://www.wikidata.org/prop/direct/> \
                 PREFIX wd: <http://www.wikidata.org/entity/> \
@@ -93,18 +86,18 @@ function(
 
             console.log(thisB._makeQuery(query));
 
-            xhr.get( this.url+'?'+ioQuery.objectToQuery({ query: thisB._makeQuery(query) }), {
+            xhr.get(this.url + '?' + ioQuery.objectToQuery({ query: thisB._makeQuery(query) }), {
                 headers: headers,
-                handleAs: "json",
+                handleAs: 'json',
                 failOk: true
-            }).then( function(o) {
-                callback(o)
-            }, function(e) {
-                setTimeout(function() {
-                    thisB._readChunk( query, callback );
+            }).then(function (o) {
+                callback(o);
+            }, function (e) {
+                setTimeout(function () {
+                    thisB._readChunk(query, callback);
                 }, query.backoff);
 
-                if(query.backoff) {
+                if (query.backoff) {
                     query.backoff *= 2;
                 } else {
                     query.backoff = 200;
